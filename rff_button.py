f6183e9285070078a02770a082e07e8bdfea28ed
Wang Huachen <huachenw24@gmail.com>
2020-03-30 12:11:51 +0800

    added in time component to detect long presses

--- rff_button.py
+++ rff_button.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import time
 from std_msgs.msg import String
 import subprocess
 
@@ -8,28 +9,49 @@ class Button():
 
 	def __init__(self):
 		self.dinput = 1
-
+		self.status = False
+		self.timedown = 0
+		self.timeup = 0
+		self.hold = 0
 		# Set publisher
 		self.pub = rospy.Publisher('buttonpress', String, queue_size = 5)
 		rospy.init_node('Button', anonymous=False)
-		self.rate = rospy.Rate(2)
-
+		self.rate = rospy.Rate(5)
+		
 	def checkPress(self):
 		while not rospy.is_shutdown():
 			try:
 				self.dinput = subprocess.check_output(["ssh", "root@192.168.1.1", "gpio.sh get DIN1"], shell=False)
 		
 				if self.dinput == "0\n":
-					self.pub.publish("Button has been pressed")
-					rospy.loginfo("Button has been pressed")
-
+					if self.status == False:
+						self.timedown = time.time()
+						rospy.loginfo("pressed: %f", self.timedown)
+					self.status = True
+					self.pub.publish(str(self.status))
+					
 				elif self.dinput == "1\n":
-					self.pub.publish("Button not pressed")
-					rospy.loginfo("Button not pressed")
+					if self.status == True:
+						self.timeup = time.time()
+						rospy.loginfo("released: %f", self.timeup)
+					self.status = False
+					self.pub.publish(str(self.status))
+					
+				else:
+					rospy.loginfo("Invalid Digital Input")
+
+				if self.timedown > self.timeup:
+					if (time.time() - self.timedown) > 1.2:
+						rospy.loginfo("Long press")
+					else:
+						pass
 
 				else:
-					self.pub.publish("Invalid Digital Input")
-					rospy.loginfo("Invalid Digital Input")
+					if self.hold == self.timeup - self.timedown:
+						pass
+					else:
+						self.hold = self.timeup - self.timedown
+						rospy.loginfo("Time held: %f", self.hold)
 
 			except(subprocess.CalledProcessError):
 				rospy.logwarn("SSH process into router terminated.")
